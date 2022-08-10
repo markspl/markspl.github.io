0d3191ec38bb6063bc80221ce7eb8756512da4bb
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-10 12:09:10 +0300

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,15 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-  // Left for example
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/:pathMatch(.*)',
+    name: '404',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/404.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>404</h1>
+  <p>[ <a href="/">Go back</a> ]</p>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HeaderItem from '@/components/Header.vue';
+
+export default {
+  components: {
+    // HeaderItem,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 50px;
+}
+</style>
